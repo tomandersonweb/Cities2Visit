879e2a78a7e7e5875d69b1fd12a34e764fe8a800
Tom Anderson <tom@andersonweb.co.uk>
2017-03-12 20:00:55 +0000

    Added view models for app and app.js intial setup

---
+++ src/Cities2Visit.Web/ViewModels/CityViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cities2Visit.Web.ViewModels
+{
+    public class CityViewModel
+    {
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public List<string> Attractions { get; set; }
+
+        public bool Visited { get; set; }
+    }
+}
